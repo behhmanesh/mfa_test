5da690a44c795f03f10a17793b4858c90cb336a9
KBL-DHR-015\Administrator <yassinbehmanish@gmail.com>
2021-04-14 09:10:58 +0430

    my project

---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ <link rel="stylesheet" type="text/css" href="{{url('public/css/style.css')}}">
+    <title>login page!</title>
+<div class="container">
+    <div class="card col-md-6 offset-md-3">
+        <div class="card-header">
+            <h1>Login</h1>
+            <div class="card-body">
+            <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">User Name</label>
+    <input type="name" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter name">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  
+  <button type="submit" class="btn btn-success">Submit</button>
+</form>
+            </div>
+        </div>
+    </div>
+</div>
+
+</head>
+<body>
+    
+</body>
+</html>
